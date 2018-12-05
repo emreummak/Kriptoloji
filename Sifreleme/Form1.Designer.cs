7cbda19814c70ec0f319340eb70a6d517c8373af
scrazyy <emreummak@gmail.com>
2018-12-05 04:44:00 +0300

    C# Sifreleme

---
+++ Sifreleme/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Sifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şifrele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 286);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ceasar Algoritması",
+            "Polybius Algoritması",
+            "Çit Algoritması",
+            "Sütun Algoritması",
+            "T. Olmayan Matris Algoritması"});
+            this.comboBox1.Location = new System.Drawing.Point(226, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(165, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Şifrelenecek Metin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifreleme Algoritması:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(436, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Şifreli Metin:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(397, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(208, 281);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(257, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Şifreleme Anahtarı:";
+            this.label4.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(226, 148);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(165, 49);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Visible = false;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 330);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Şifrele";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Sifreleme/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sifreleme
+{
+    public partial class Form1 : Form
+    {
+        public string ceasar(string metin,int say) {
+            string alfabe = "abcçdefgğhıijklmnoöprsştuüvyz";
+            string sifreli = "";
+            for (int i=0; i<metin.Length; i++) {
+                for (int j=0; j<alfabe.Length; j++) {
+                    if (metin[i]==alfabe[j]) {
+                        int gecici = (j + say > 28) ? (j + say) - 29 : j + say;
+                        sifreli = sifreli + alfabe[gecici];
+                    }
+                }
+            }
+            return sifreli;
+        }
+        public string polybius(string metin) {
+            int [,]dizi = new int[5,5];
+            string sifreli = "";
+            string alfabe = "abcdefgğhıjklmnoprsştuvyz";
+            int say = 0;
+            for (int i=0;i<5;i++) {
+                for (int j=0;j<5;j++) {
+                    dizi[i,j] = alfabe[say];
+                    say++;
+                }
+            }
+            for (int i=0;i<metin.Length;i++) {
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (metin[i]==dizi[j,k]) {
+                            sifreli = sifreli + (j+1) + (k+1);
+                        }                        
+                    }
+                }
+            }
+            return sifreli;
+        }
+        public string cit(string metin) {
+            string tek = "";
+            string cift = "";
+            for (int i=0;i<metin.Length;i++) {
+                if (i % 2 == 0)
+                {
+                    tek = tek + metin[i];
+                }
+                else
+                {
+                    cift = cift + metin[i];
+                }
+            }
+            return metin.Length%2!=0?(tek.Length>cift.Length?tek+cift:cift+tek):tek+cift;
+        }
+        public string sutun(string metin, int stn) {
+            Random rnd = new Random();
+            string alfabe = "abcçdefgğhıijklmnoöprsştuüvyz";
+            metin = metin.Trim();
+            int mBoy = 0;
+            int rndsayisi = 0;
+            if (metin.Length % stn != 0)
+            {
+                mBoy = ((metin.Length / stn) + 1)*stn;
+            }
+            if (metin.Length<mBoy) {
+                rndsayisi = mBoy - metin.Length;
+                for (int i=0;i<rndsayisi;i++) {
+                    metin += alfabe[rnd.Next(0,28)];
+                }
+            }
+            char[,] dizi = new char[metin.Length/stn, stn];
+            int sayac = 0;
+            string sifreli = "";
+            for (int i=0;i<metin.Length/stn;i++) {
+                for (int j=0;j<stn;j++) {
+                    dizi[i, j] = metin[sayac];
+                    sayac++;
+                }
+            }
+            for (int i = 0; i < stn; i++)
+            {
+                for (int j = 0; j < metin.Length/stn; j++)
+                {
+                    sifreli += dizi[j, i];
+                }
+            }
+            return sifreli;
+        }
+        public string matris(string metin) {
+            int[,] det1 = new int[3,3] { { 3, 2, 2 }, { 0, 1,0 }, { 1,0,1 } };
+            string alfabe = " abcçdefgğhıijklmnoöprsştuüvyz";
+            int mBoy = 0;
+            int ssay = 0;
+            mBoy = ((metin.Length / 3) + 1) * 3;
+            ssay = mBoy - metin.Length;
+            if (metin.Length % 3 != 0)
+            {
+                for (int i = 0; i < ssay; i++)
+                {
+                    metin += "0";
+                }
+            }
+            int sutun = mBoy / 3;
+            int[,] dizi = new int[3, sutun];
+            string sifreli="";
+            int[] sifreliDizi = new int[mBoy];
+            int sayac = 0;
+            for (int i=0;i<metin.Length;i++) {
+                for (int j=0;j<alfabe.Length;j++) {
+                    if (metin[i]==alfabe[j]) {
+                        sifreliDizi[sayac]=j;
+                        sayac++;
+                    }
+                }
+            }
+            sayac = 0;
+            for (int i=0;i<3;i++) {
+                for (int j=0;j<sutun;j++) {
+                    dizi[i, j] = sifreliDizi[sayac];
+                    sayac++;
+                }
+            }
+            int[,] carpim = new int[3, sutun];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        carpim[i, j] += det1[i, k] * dizi[k, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < sutun; j++)
+                {
+                    sifreli += carpim[i, j];
+                }
+            }
+            return sifreli;
+        } 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sMetin = textBox1.Text;
+            string aMetin = textBox3.Text; if (aMetin=="") { aMetin ="1"; }
+            if (comboBox1.Text == "Ceasar Algoritması") {
+                textBox2.Text = ceasar(sMetin, Int32.Parse(aMetin));
+            }
+            else if (comboBox1.Text == "Polybius Algoritması") {
+                textBox2.Text = polybius(sMetin);
+            }
+            else if (comboBox1.Text == "Çit Algoritması")
+            {
+                textBox2.Text = cit(sMetin);
+            }
+            else if (comboBox1.Text == "Sütun Algoritması")
+            {
+                textBox2.Text = sutun(sMetin,Int32.Parse(aMetin));
+            }
+            else if (comboBox1.Text == "T. Olmayan Matris Algoritması")
+            {
+                textBox2.Text = matris(sMetin);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Ceasar Algoritması" || comboBox1.Text == "Sütun Algoritması")
+            {
+                label4.Visible = true;
+                button1.Visible = true;
+                textBox3.Visible = true;
+            }
+            else {
+                label4.Visible = false;
+                button1.Visible = true;
+                textBox3.Visible = false;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+               && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToLower();
+            textBox1.SelectionStart = textBox1.Text.Length;
+        }
+    }
+}
